24c01590c82ca6ed4caab9a39ba23aa316c0da1b
中嶋萌花 <moeka0531tf@gmail.com>
2020-05-04 14:05:10 +0900

    add detect emotion sample

--- azure_sample/face_api.py
+++ azure_sample/face_api.py
@@ -16,6 +16,11 @@ from azure.cognitiveservices.vision.face import FaceClient
 from msrest.authentication import CognitiveServicesCredentials
 from azure.cognitiveservices.vision.face.models import TrainingStatusType, Person, SnapshotObjectType, OperationStatusType
 
+# 感情値の出力
+def getEmotion(faceDictionary):
+    print(faceDictionary.face_attributes.emotion)
+
+
 if __name__ == "__main__":
 
     # 環境変数の読み込み
@@ -24,3 +29,16 @@ if __name__ == "__main__":
 
     KEY = os.environ.get("FACE_SUBSCRIPTION_KEY")
     FACE_ENDPOINT = os.environ.get("FACE_ENDPOINT")
+
+    # faceClientの作成
+    face_client = FaceClient(FACE_ENDPOINT, CognitiveServicesCredentials(KEY))
+
+    # 顔の向きを取得する
+    image_url = 'https://raw.githubusercontent.com/Microsoft/Cognitive-Face-Windows/master/Data/detection1.jpg'
+    image_name = os.path.basename(image_url) # os.path.name -> 拡張子を含むファイル名を取得
+    detected_faces = face_client.face.detect_with_url(url=image_url, return_face_attributes=['emotion'])
+    if not detected_faces:
+        raise Exception('No face detected from image {}'.format(image_name))
+
+    for face in detected_faces:
+        getEmotion(face)
