db80fceb0c81c658de8b91036c741e495d1f0fe5
中嶋萌花 <moeka0531tf@gmail.com>
2020-05-04 15:04:01 +0900

    remove unused import

--- azure_sample/face_api.py
+++ azure_sample/face_api.py
@@ -1,20 +1,9 @@
-import asyncio
-import io
-import glob
 import os
 from os.path import join
-import sys
-import time
-import uuid
-import requests
 from dotenv import load_dotenv
 from pathlib import Path
-from urllib.parse import urlparse
-from io import BytesIO
-from PIL import Image, ImageDraw
 from azure.cognitiveservices.vision.face import FaceClient
 from msrest.authentication import CognitiveServicesCredentials
-from azure.cognitiveservices.vision.face.models import TrainingStatusType, Person, SnapshotObjectType, OperationStatusType
 
 # 感情値の出力
 def getEmotion(faceDictionary):
