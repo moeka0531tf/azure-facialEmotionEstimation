135ceae74518bc3ba189187b1249b7449f8b3a7f
中嶋萌花 <moeka0531tf@gmail.com>
2020-05-05 18:09:08 +0900

    add facialEmotionEstimation feature

---
+++ facialEmotionEstimation/azure_api_call.py
@@ -0,0 +1,34 @@
+import os
+from os.path import join
+from dotenv import load_dotenv
+from pathlib import Path
+from azure.cognitiveservices.vision.face import FaceClient
+from msrest.authentication import CognitiveServicesCredentials
+import ast
+
+
+def prepare():
+
+    # 環境変数の読み込み
+    dotenv_path = join(Path(os.getcwd()).parent, '.env')
+    load_dotenv(dotenv_path)
+
+    FACE_SUBSCRIPTION_KEY = os.environ.get("FACE_SUBSCRIPTION_KEY")
+    FACE_ENDPOINT = os.environ.get("FACE_ENDPOINT")
+
+    # faceClientの作成
+    return FaceClient(FACE_ENDPOINT, CognitiveServicesCredentials(FACE_SUBSCRIPTION_KEY))
+
+
+def get_emotion(image_url, face_client):
+
+    # 表情を取得する
+    image_url = image_url
+    image_file_name = os.path.basename(image_url)
+    detected_faces = face_client.face.detect_with_url(url=image_url, return_face_attributes=['emotion'])
+    if not detected_faces:
+        raise Exception('No face detected from image {}'.format(image_file_name))
+
+    # MEMO1: azureのEmotionオブジェクト -> str -> dict型へ変換
+    # MEMO2: image内には一人しか映っていない想定
+    return ast.literal_eval(str(detected_faces[0].face_attributes.emotion))
---
+++ facialEmotionEstimation/draw_graph.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import plotly.express as px
+
+
+def shape_data(emotion_dict):
+
+    # label,valueを取得しDataFrameへと整形
+    label = ['anger', 'contempt', 'disgust', 'fear', 'happiness', 'neutral', 'sadness', 'surprise']
+    value = []
+
+    for i in label:
+        value.append(emotion_dict[i])
+
+    df = pd.DataFrame(value, index=label, columns=['value'])
+    print(df)
+
+    return df, label, value
+
+
+def draw(emotion_dict):
+    df, label, value = shape_data(emotion_dict)
+
+    # グラフ出力
+    fig = px.bar(data_frame=df, x=label, y=value, color=label, width=640, height=360,
+                 title='emotion value')
+    fig.update_xaxes(title_text='emotion')
+    fig.update_yaxes(title_text='score', range=[0, 1])
+    fig.update_layout(showlegend=False, title_x=0.5)
+    fig.show()
